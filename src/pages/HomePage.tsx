d8abfdfac0283b296268259e21fbe8e8a07ec052
NepSauce <greasyandmoist@gmail.com>
2026-02-05 01:08:44 -0400

    Feat: Add basic homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    const name = "Zawad Atif";
+
+    return (
+        <div>
+            <h1>Welcome to {name}'s Portfolio</h1>
+        </div>
+    );
+}
+
+export default HomePage;
